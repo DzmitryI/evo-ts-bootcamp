c517fadbb7cf420e25054e3474ba65f89ca687b6
Dzmitry Ivanou <3prn@mail.ru>
2021-04-12 02:18:15 +0300

    feat: implement search box

--- homeworks/02-images-loader/src/App.tsx
+++ homeworks/02-images-loader/src/App.tsx
@@ -4,11 +4,40 @@ import styles from './App.module.css'
 
 type AppState = {
 	arrOfImg: {alt_description: string, height: number, urls: {}, width: number}[],
+	query: string,
 }
 
 export default class App extends Component<{}, AppState> {
+
 	state = {
 		arrOfImg: [],
+		query: '',
+	}
+
+	onClick = async () => {
+		const {query} = this.state;
+		try {
+			const result: {data:  {results: {alt_description: string, height: number, urls: {}, width: number}[]}} = await api.get('/search/photos', {params:{query, per_page: 30}})
+			this.setState({arrOfImg: result.data.results})
+		} catch (e) {
+			alert(e.message)
+		}
+	}
+
+	onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({query: e.currentTarget.value.trim()})
+	}
+
+	handleKeyPress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if(e.key === 'Enter'){
+			const {query} = this.state;
+			try {
+				const result: {data:  {results: {alt_description: string, height: number, urls: {}, width: number}[]}} = await api.get('/search/photos', {params:{query, per_page: 30}})
+				this.setState({arrOfImg: result.data.results})
+			} catch (e) {
+				alert(e.message)
+			}
+		}
 	}
 
 	async componentDidMount() {
@@ -21,12 +50,18 @@ export default class App extends Component<{}, AppState> {
 	}
 
 	render() {
-		const {arrOfImg} = this.state;
+		const {arrOfImg, query} = this.state;
 		return (
 			<div className={styles.app}>
 				<div className={styles.searchPanel}>
-					<input type="text" className={styles.search}/>
-					<button className="btn">search</button>
+					<input
+						type="text"
+						className={styles.search}
+						value={query}
+						onChange={this.onChange}
+						onKeyPress={this.handleKeyPress}
+					/>
+					<button className={styles.btn} onClick={this.onClick}>search</button>
 				</div>
 				<body className={styles.imagesContainer}>
 				{arrOfImg.length > 0 ? arrOfImg.map((curImg, idx) => {
