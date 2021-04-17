cf9e399185b37106f44176853728ab626ae8cacb
Dzmitry Ivanou <3prn@mail.ru>
2021-04-17 13:28:02 +0300

    init: start 01-bubble-sort

---
+++ homeworks/01-bubble-sort/src/components/button/Button.tsx
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+type ButtonProps = {
+	name: string,
+	isDisabled: boolean,
+	action: () => void,
+}
+
+class Button extends Component<ButtonProps, {}> {
+
+	onClick = () => {
+		this.props.action()
+	}
+
+	render() {
+		const {isDisabled, name} = this.props;
+		return (
+			<button onClick={this.onClick} disabled={isDisabled}>
+				{name}
+			</button>
+		);
+	}
+}
+
+export default Button;
